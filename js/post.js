f0f66f12666f6f29171bbeab653be0a1473a90a5
Claudio Cortese <claudio.cortese@>
2024-10-23 15:40:27 +0200

    Add back to top button

--- js/post.js
+++ js/post.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const scrollToTopBtn = document.getElementById("scrollToTopBtn")
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
     const details = document.querySelector('details');
     const rootElement = document.documentElement;
+    let hasCssClassesBeenAdded = false;
 
     function scrollToTop () {
         rootElement.scrollTo({
@@ -13,10 +14,23 @@ document.addEventListener('DOMContentLoaded', () => {
     function handleScroll() {
         const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
         
-        if ((rootElement.scrollTop / scrollTotal ) > 0.80 ) {
+        if ((rootElement.scrollTop / scrollTotal ) > 0.40 ) {
             scrollToTopBtn.classList.add("showBtn")
+            
+            if(!hasCssClassesBeenAdded) {
+                setTimeout(() => {
+                    scrollToTopBtn.firstElementChild.classList.add("animate__animated", "animate__bounce");
+                    hasCssClassesBeenAdded = true;
+                }, 300);
+
+                scrollToTopBtn.firstElementChild.addEventListener("animationend", () => {
+                    scrollToTopBtn.firstElementChild.classList.remove("animate__animated", "animate__bounce");
+                });
+            }
         } else {
-        scrollToTopBtn.classList.remove("showBtn")
+            scrollToTopBtn.classList.remove("showBtn");
+            scrollToTopBtn.firstElementChild.classList.remove("animate__animated", "animate__bounce");
+            hasCssClassesBeenAdded = false;
         }
     }
 
