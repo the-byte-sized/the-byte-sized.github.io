5b5d362071ecf3981463179266fcccb485317022
Claudio Cortese <claudio.cortese@>
2024-10-23 13:03:17 +0200

    Add scroll to top button to post layout

--- js/post.js
+++ js/post.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn")
     const details = document.querySelector('details');
+    const rootElement = document.documentElement;
+
+    function scrollToTop () {
+        rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        })
+    }
+
+    function handleScroll() {
+        const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+        
+        if ((rootElement.scrollTop / scrollTotal ) > 0.80 ) {
+            scrollToTopBtn.classList.add("showBtn")
+        } else {
+        scrollToTopBtn.classList.remove("showBtn")
+        }
+    }
+
+    document.addEventListener("scroll", handleScroll);
+
+    scrollToTopBtn?.addEventListener("click", scrollToTop);
 
     if(!details?.querySelector('ul')) {
         details.classList.add('hidden');
