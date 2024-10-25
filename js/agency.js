bc5b1fb9d33676eebeeb1b915cfe8a48761bc0f9
Claudio Cortese <claudio.cortese@>
2024-10-25 12:53:36 +0200

    Fix navbar

--- js/agency.js
+++ js/agency.js
@@ -46,4 +46,27 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('[data-lazy-load-bg]').forEach((element) => {
         lazyLoadedBgsObserver.observe(element);
     });
+    
+    const servicesSection = document.querySelector('header');
+    const nav = document.querySelector('nav');
+    
+    const callback = ((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting === false) {
+                nav.style.setProperty('transform', 'translateY(0px)');
+                nav.style.setProperty('background-color', 'rgb(0 0 0 / 0.75)');
+            } else {
+                nav.style.setProperty('transform', 'translateY(30px)');
+                nav.style.setProperty('background-color', 'transparent');
+            }
+        })        
+    });
+    
+    const observer = new IntersectionObserver(callback, {
+        rootMargin: "-120px 0px 0px 0px"
+    })
+    
+    observer.observe(servicesSection);
+    
+    console.log(servicesSection);
 })
